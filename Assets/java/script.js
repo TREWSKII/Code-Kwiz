c340fdbe8825cf8b0020f7bde7767f51ae21bbaa
Drew <dlederman69@gmail.com>
2022-07-17 23:21:46 -0600

    initail commit

---
+++ Assets/java/script.js
@@ -0,0 +1,26 @@
+// TODO: I WANT to take a timed quiz on JavaScript fundamentals that stores high scores SO THAT I can gauge my progress compared to my peers
+
+
+
+// TODO: start the quiz by ckicking the button the timer starts and the first question appears 
+
+
+
+//TODO: after the first question we need to presented with the next, so on and so forth....
+
+
+
+//TODO: when a question is answered incorrectly 15 seconds is subtracted from the clock
+// ! if you google the function .getTime it subtracts 15 seconds from the inccorect question
+
+
+
+//TODO:all questions are answered or the timer reaches 0 - THEN the game is over
+
+
+
+//TODO: then i save my initals and score - 
+
+
+
+
